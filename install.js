1e374bd6efb187d386c22bb62b411246c13cc051
taoyuan <torworx@gmail.com>
2014-09-12 14:52:09 +0800

    update

--- install.js
+++ install.js
@@ -6,7 +6,7 @@ config.silent = true;
 
 require('colors');
 
-console.log('Detecting the OS Distributor'.bold());
+console.log('Detecting the OS Distributor'.bold);
 var distributor = sh.exec('lsb_release -a | grep Distributor\\ ID').output;
 try {
     distributor = distributor.split(':')[1].trim().toLowerCase();
@@ -15,7 +15,7 @@ try {
     process.exit(1);
 }
 
-console.log('Detecting the user'.bold());
+console.log('Detecting the user'.bold);
 var user = sh.exec('users').output;
 try {
     user = user.split('\t\r\n');
@@ -24,16 +24,16 @@ try {
     process.exit(1);
 }
 
-console.log('Create the Dolink Setup Folder'.bold());
+console.log('Create the Dolink Setup Folder'.bold);
 sh.rm('-fr', '/opt/setup');
 sh.mkdir('-p', '/opt/setup');
 
-console.log('Fetching the Setup Repo from Github'.bold());
+console.log('Fetching the Setup Repo from Github'.bold);
 sh.exec('git clone https://github.com/dolink/setup.git /opt/setup ');
 sh.cd('/opt/setup');
 sh.exec('git checkout master');
 
-console.log(('Set ' + user + ' user as the owner of this directory').bold());
+console.log(('Set ' + user + ' user as the owner of this directory').bold);
 sh.exec('sudo chown -R ' + user + ' /opt/');
 
 require('./scripts/' + distributor + '/install');
