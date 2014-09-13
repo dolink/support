22e7b1862874d431dde4cbb8ab5d14aaeb39a5bb
taoyuan <torworx@gmail.com>
2014-09-13 23:38:52 +0800

    update

--- scripts/pi/sn.js
+++ scripts/pi/sn.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 var sh = require('shelljs');
+var parser = require('properties-parser');
 
 module.exports = function () {
     // sudo grep Serial /proc/cpuinfo | cut -d: -f 2 | tr -d ' ' |  tr '[:lower:]' '[:upper:]' > /etc/agent/serial.conf
-    var serial = sh.exec('grep Serial /proc/cpuinfo').output;
-    serial.split(':')[1].trim().toLowerCase().to('/etc/agent/serial.conf');
+    var props = parser.parse(sh.exec('grep Serial /proc/cpuinfo').output);
+    props['Serial'].toLowerCase().to('/etc/agent/serial.conf');
 };
