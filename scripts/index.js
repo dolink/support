47a44aec93abf78ab0da9462ee2831e133d56dc2
taoyuan <torworx@gmail.com>
2014-09-13 22:57:18 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,9 @@
 
 var p = require('../lib/platform').product;
 
-exports.sn = require(p + '/sn');
+var path = require('path');
+function plmod(name) {
+    return require(path.join(__dirname, p, name));
+}
+
+exports.sn = plmod('sn');
