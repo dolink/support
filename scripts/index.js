91b66afca092ecb751aacdf87963559cf1251bd2
taoyuan <torworx@gmail.com>
2014-09-13 22:12:25 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,3 @@
 "use strict";
 
-var _ = require('lodash');
-var needs = require('needs');
-var path = require('path');
-var platform = require('platforms').detect();
-
-//_.assign(exports, needs(path.join(__dirname, platform)));
-exports.sn = require(platform + '/sn');
+//exports.sn = require(platform + '/sn');
